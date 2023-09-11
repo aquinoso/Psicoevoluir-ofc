a05efb5207073aeb73b0fae2e681fdfee02641b6
aquinoso <140344443+aquinoso@users.noreply.github.com>
2023-09-11 12:59:32 -0300

    Upload primário

---
+++ js/script.js
@@ -0,0 +1,149 @@
+/**
+ * WEBSITE: https://themefisher.com
+ * TWITTER: https://twitter.com/themefisher
+ * FACEBOOK: https://www.facebook.com/themefisher
+ * GITHUB: https://github.com/themefisher/
+ */
+
+// Preloader js
+$(window).on("load", function () {
+  "use strict";
+  $(".preloader").fadeOut(0);
+});
+
+(function ($) {
+  "use strict";
+
+  // tab
+  $(".tab-content")
+    .find(".tab-pane")
+    .each(function (idx, item) {
+      var navTabs = $(this).closest(".code-tabs").find(".nav-tabs"),
+        title = $(this).attr("title");
+      navTabs.append(
+        '<li class="nav-item"><a class="nav-link" href="#">' +
+          title +
+          "</a></li>"
+      );
+    });
+
+  $(".code-tabs ul.nav-tabs").each(function () {
+    $(this).find("li:first").addClass("active");
+  });
+
+  $(".code-tabs .tab-content").each(function () {
+    $(this).find("div:first").addClass("active");
+  });
+
+  $(".nav-tabs a").click(function (e) {
+    e.preventDefault();
+    var tab = $(this).parent(),
+      tabIndex = tab.index(),
+      tabPanel = $(this).closest(".code-tabs"),
+      tabPane = tabPanel.find(".tab-pane").eq(tabIndex);
+    tabPanel.find(".active").removeClass("active");
+    tab.addClass("active");
+    tabPane.addClass("active");
+  });
+
+  // accordion-collapse
+  $(".accordion-collapse").on("show.bs.collapse", function () {
+    $(this).siblings(".accordion-header").addClass("active");
+  });
+  $(".accordion-collapse").on("hide.bs.collapse", function () {
+    $(this).siblings(".accordion-header").removeClass("active");
+  });
+
+  //post slider
+  $(".post-slider").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4500,
+    dots: false,
+    arrows: true,
+    prevArrow:
+      '<button type="button" class="prevArrow"><i class="fas fa-angle-left"></i></button>',
+    nextArrow:
+      '<button type="button" class="nextArrow"><i class="fas fa-angle-right"></i></button>',
+  });
+
+  // videoPopupInit
+  function videoPopupInit() {
+    var $videoSrc;
+    $(".video-play-btn").click(function () {
+      $videoSrc = $(this).data("src");
+    });
+    $("#videoModal").on("shown.bs.modal", function (e) {
+      $("#showVideo").attr(
+        "src",
+        $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0"
+      );
+    });
+    $("#videoModal").on("hide.bs.modal", function (e) {
+      $("#showVideo").attr("src", $videoSrc);
+    });
+  }
+  videoPopupInit();
+
+  // table of content
+  new ScrollMenu("#TableOfContents a", {
+    duration: 400,
+    activeOffset: 40,
+    scrollOffset: 10,
+  });
+})(jQuery);
+
+
+
+
+
+
+
+
+
+
+
+
+
+const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
+
+const textArray = ["felizes!", "engajados!", "produtivos!"];
+const typingDelay = 200;
+const erasingDelay = 100;
+const newTextDelay = 2000; // Delay between current and next text
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+    setTimeout(erase, newTextDelay);
+  }
+}
+
+function erase() {
+  if (charIndex > 0) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+    textArrayIndex++;
+    if(textArrayIndex>=textArray.length) textArrayIndex=0;
+    setTimeout(type, typingDelay + 1100);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
+  if(textArray.length) setTimeout(type, newTextDelay + 250);
+});
